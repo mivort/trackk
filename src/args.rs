71fafb7c183b6937e0fdc9cbc9517ff2120916fb
Mivort <lufterd@gmail.com>
2025-05-13 19:31:24 +0100

    feat: add cli arguments

--- src/args.rs
+++ src/args.rs
@@ -14,4 +14,76 @@ pub struct Args {
 }
 
 #[derive(Subcommand)]
-pub enum Command {}
+pub enum Command {
+    /// Create new entry
+    #[command(visible_aliases(["a"]))]
+    Add(EntryArgs),
+
+    /// Remove specified entry
+    #[command(visible_aliases(["rem", "rm", "r", "delete", "del", "d"]))]
+    Remove(FilterArgs),
+
+    /// Modify specified entry
+    #[command(visible_aliases(["mod", "m"]))]
+    Modify(ModArgs),
+
+    /// List entries using set of filters
+    #[command(visible_aliases(["ls", "l"]))]
+    List(FilterArgs),
+
+    /// Show info about specified entry
+    #[command(visible_aliases(["inf", "i"]))]
+    Info(FilterArgs),
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self::List(FilterArgs::default())
+    }
+}
+
+#[derive(Parser, Default)]
+pub struct FilterArgs {
+    /// Entry reference (UUID, number, latest)
+    pub id: Option<String>,
+
+    /// Filter by entry title content
+    #[arg(long)]
+    pub contains: Option<String>,
+
+    /// Filter by max due date
+    #[arg(long)]
+    pub due_before: Option<String>,
+
+    /// Filter by min due date
+    #[arg(long)]
+    pub due_after: Option<String>,
+
+    /// Filter by status
+    #[arg(long)]
+    pub has_status: Option<String>,
+}
+
+#[derive(Parser)]
+pub struct EntryArgs {
+    /// Entry title
+    #[arg(short('m'), visible_aliases(["message", "msg"]), long)]
+    pub title: Option<String>,
+
+    /// Entry due reference
+    #[arg(short, long)]
+    pub due: Option<String>,
+
+    /// Entry status
+    #[arg(short, long)]
+    pub status: Option<String>,
+}
+
+#[derive(Parser)]
+pub struct ModArgs {
+    #[command(flatten)]
+    pub filter: FilterArgs,
+
+    #[command(flatten)]
+    pub entry: EntryArgs,
+}
