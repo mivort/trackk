c421c733168b95d9d9d02ff21ad43713ce9a9e14
Mivort <lufterd@gmail.com>
2025-05-22 19:35:37 +0100

    feat: implement simple issue markdown formatting

--- src/editor.rs
+++ src/editor.rs
@@ -1,14 +1,22 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use crate::args::FilterArgs;
 use crate::config::Config;
-use crate::prelude::*;
+use crate::issue::Issue;
+use crate::{prelude::*, storage};
 
 /// Iterate over matching entries and run editor for each.
-pub fn edit_entries(_filter: &FilterArgs, config: &Config) -> Result<()> {
-    Command::new(&config.editor).output()?;
+pub fn edit_entries(filter: &FilterArgs, config: &Config) -> Result<()> {
+    let entries = storage::fetch_entries(filter, config)?;
+    for (issue, _path) in &entries {
+        let _format = format_markdown(&issue);
+    }
 
-    // TODO: output issue in editor-friendly format
+    if false {
+        Command::new(&config.editor)
+            .stdin(Stdio::inherit())
+            .spawn()?;
+    }
 
     Ok(())
 }
@@ -24,7 +32,22 @@ pub fn edit_entries(_filter: &FilterArgs, config: &Config) -> Result<()> {
 /// * Field 1: value
 /// * Field 2: value
 /// ```
-fn _format_markdown() {}
+fn format_markdown(issue: &Issue) {
+    let tags = issue.tags.iter().map(|t| &**t).collect::<Vec<_>>();
+
+    println!(
+        concat!(
+            "{title}\n\n----\n\n",
+            "* Status: {status}\n",
+            "* Due: {due}\n",
+            "* Tags: {tags}\n",
+        ),
+        title = issue.title,
+        status = issue.status,
+        due = issue.due.unwrap_or_default(),
+        tags = tags.join(" "),
+    );
+}
 
 /// Read edited entry back to the issue struct.
 fn _parse_markdown() {}
