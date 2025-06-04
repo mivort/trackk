a9f30adeb19997df4ca8185acf985880361043ff
Mivort <lufterd@gmail.com>
2025-06-05 00:49:26 +0100

    feat(display): update row display

--- src/display.rs
+++ src/display.rs
@@ -5,18 +5,16 @@ use crate::issue::Issue;
 /// Render the list of filtered entries.
 pub fn show_entries(entries: &[(Issue, Rc<str>)]) {
     for (issue, _path) in entries {
+        let tags = issue.tags.iter().map(|t| format!("@{}", t));
+        let tags = tags.collect::<Vec<_>>().join(" ");
+
         let title = issue.title.lines().next().unwrap_or_default();
         let status = &issue.status[0..1];
         println!(
-            " {short}. [{status}] {id}: {title} ({tags})",
+            " {short}. [{status}] {id}: {tags}{tags_space}{title}",
             id = issue.id,
             short = issue.short.unwrap_or(0),
-            tags = issue
-                .tags
-                .clone()
-                .into_iter()
-                .collect::<Vec<String>>()
-                .join(" "),
+            tags_space = if tags.is_empty() { "" } else { " " }
         );
     }
 }
