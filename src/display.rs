bcd2fd77dc5058b89576c49808be349f1f8ad638
Mivort <lufterd@gmail.com>
2025-06-04 22:41:39 +0100

    feat(display): show more data in output

--- src/display.rs
+++ src/display.rs
@@ -4,12 +4,14 @@ use crate::issue::Issue;
 
 /// Render the list of filtered entries.
 pub fn show_entries(entries: &[(Issue, Rc<str>)]) {
-    for (issue, path) in entries {
+    for (issue, _path) in entries {
         let title = issue.title.lines().next().unwrap_or_default();
+        let status = &issue.status[0..1];
         println!(
-            "[{short}] {id}: {path}: {title}",
+            " {short}. [{status}] {id}: {title} ({tags})",
             id = issue.id,
-            short = issue.short.unwrap_or(0)
+            short = issue.short.unwrap_or(0),
+            tags = issue.tags.clone().into_iter().collect::<Vec<String>>().join(" "),
         );
     }
 }
