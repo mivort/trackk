a6ac11c3c058cf8dfd15896030d0fddee5d10ee5
Mivort <lufterd@gmail.com>
2025-06-05 19:29:50 +0100

    feat(dateexp): add basic support for number suffixes

--- src/dateexp.rs
+++ src/dateexp.rs
@@ -1,3 +1,5 @@
+use std::num::ParseFloatError;
+
 use nom::character::complete::char;
 use nom::character::complete::digit1;
 use nom::combinator::{map_res, opt, recognize};
@@ -10,15 +12,34 @@ pub fn parse_date(input: &str) -> Result<i64> {
     let (_, num) = unwrap_ok_or!(parse_number(input), e, {
         bail!("Date parsing error: {:?}", e)
     });
-    Ok((num * 10.) as i64)
+    Ok(num as i64)
 }
 
 /// Convert float point string into f64.
 fn parse_number(input: &str) -> IResult<&str, f64> {
-    map_res(recognize_float, |s: &str| s.parse::<f64>()).parse(input)
+    map_res(
+        (recognize_float, nom::character::complete::alpha0),
+        |(s, suffix): (&str, &str)| {
+            Ok::<_, ParseFloatError>(s.parse::<f64>()? * match_suffix(suffix))
+        },
+    )
+    .parse(input)
 }
 
 /// Recognize float point number pattern.
 fn recognize_float(input: &str) -> IResult<&str, &str> {
     recognize((digit1, opt((char('.'), digit1)))).parse(input)
+}
+
+/// Convert number suffix to the seconds.
+fn match_suffix(suffix: &str) -> f64 {
+    match suffix {
+        "s" => 1.,
+        "m" => 60.,
+        "h" => 3600.,
+        "d" | "D" => 86400.,
+        "M" => 2592000.,
+        "y" | "Y" => 946080000.,
+        _ => 1.,
+    }
 }
