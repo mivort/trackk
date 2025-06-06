f1e632f57f4c9f039cc3c0eba0dfd7a24c5e34bc
Mivort <lufterd@gmail.com>
2025-06-06 12:07:21 +0100

    feat(dateexp): produce a token stream

--- src/dateexp.rs
+++ src/dateexp.rs
@@ -1,7 +1,8 @@
 use std::num::ParseFloatError;
 
 use nom::character::complete::char;
-use nom::character::complete::digit1;
+use nom::character::complete::{alpha0, digit1};
+use nom::combinator::iterator;
 use nom::combinator::{map_res, opt, recognize};
 use nom::{IResult, Parser};
 
@@ -9,20 +10,36 @@ use crate::prelude::*;
 
 /// Parse date expression and produce the timestamp.
 pub fn parse_date(input: &str) -> Result<i64> {
-    let (_, num) = unwrap_ok_or!(parse_number(input), e, {
-        bail!("Date parsing error: {:?}", e)
+    let mut output = Vec::<Token>::new();
+    let _op_stack = Vec::<Token>::new();
+
+    for tok in iterator(input, parse_number) {
+        match tok {
+            Token::Duration(_) | Token::_Date(_) => output.push(tok),
+            _ => {}
+        }
+    }
+
+    let out = output.iter().cloned().reduce(|v, s| {
+        if let Token::Duration(v) = v {
+            if let Token::Duration(s) = s {
+                return Token::Duration(v + s);
+            }
+        }
+        Token::Duration(0.)
     });
-    Ok(num as i64)
+
+    if let Token::Duration(out) = out.unwrap_or(Token::Duration(0.)) {
+        return Ok(out as i64);
+    }
+    bail!("Not a number");
 }
 
 /// Convert float point string into f64.
-fn parse_number(input: &str) -> IResult<&str, f64> {
-    map_res(
-        (recognize_float, nom::character::complete::alpha0),
-        |(s, suffix): (&str, &str)| {
-            Ok::<_, ParseFloatError>(s.parse::<f64>()? * match_suffix(suffix))
-        },
-    )
+fn parse_number(input: &str) -> IResult<&str, Token> {
+    map_res((recognize_float, alpha0), |(s, suffix): (&str, &str)| {
+        Ok::<_, ParseFloatError>(match_suffix(s.parse::<f64>()?, suffix))
+    })
     .parse(input)
 }
 
@@ -32,15 +49,27 @@ fn recognize_float(input: &str) -> IResult<&str, &str> {
 }
 
 /// Convert number suffix to the seconds.
-fn match_suffix(suffix: &str) -> f64 {
+fn match_suffix(literal: f64, suffix: &str) -> Token {
     match suffix {
-        "s" => 1.,
-        "m" => 60.,
-        "h" => 3600.,
-        "d" | "D" => 86400.,
-        "w" | "W" => 604800.,
-        "M" => 2592000.,
-        "y" | "Y" => 946080000.,
-        _ => 1.,
+        "" => Token::Duration(literal * 1.),
+        "s" => Token::Duration(literal * 1.),
+        "m" => Token::Duration(literal * 60.),
+        "h" => Token::Duration(literal * 3600.),
+        "d" | "D" => Token::Duration(literal * 86400.),
+        "w" | "W" => Token::Duration(literal * 604800.),
+        "M" => Token::Duration(literal * 2592000.),
+        "y" | "Y" => Token::Duration(literal * 946080000.),
+        _ => panic!("Unknown number suffix: {}", suffix),
     }
+}
+
+/// Parsed token types.
+#[derive(Clone, Copy)]
+enum Token {
+    Duration(f64),
+    _Date(i64),
+    _Add,
+    _Sub,
+    _Mul,
+    _Div,
 }
