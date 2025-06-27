0122037dd2e74d05587f557318fe07ce2d41cf59
Mivort <lufterd@gmail.com>
2025-06-27 13:51:21 +0100

    test(storage): use testdir to use new directory each time

--- tests/storage.rs
+++ tests/storage.rs
@@ -1,12 +1,10 @@
-use std::env;
-
 use assert_cmd::Command;
 
 const BIN_NAME: &str = "trk";
 
 #[cfg(test)]
 fn cmd_base() -> Command {
-    let dir = env!("CARGO_TARGET_TMPDIR");
+    let dir = testdir::testdir!();
     let mut cmd = Command::cargo_bin(BIN_NAME).unwrap();
     cmd.env("TRACKK_DATA", dir).env("TRACKK_CONFIG", "");
     cmd
@@ -20,6 +18,13 @@ fn init_storage() {
 
     let mut cmd = cmd_base();
     cmd.args(&["all"]);
+    cmd.assert().success();
 
+    let mut cmd = cmd_base();
+    cmd.args(&["add", "test entry"]);
+    cmd.assert().success();
+
+    let mut cmd = cmd_base();
+    cmd.args(&["1", "mod", "--status=deleted"]);
     cmd.assert().success();
 }
