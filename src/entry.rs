c3c5cd40cf7c679f2e4b91314c7dfa317762af47
Mivort <lufterd@gmail.com>
2025-06-27 21:50:57 +0100

    fix(entry): exclude empty 'when' from serialization

--- src/entry.rs
+++ src/entry.rs
@@ -53,6 +53,7 @@ pub struct Entry {
 
     /// Date of planned completion.
     #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub when: Option<i64>,
 
     /// Due date/time.
