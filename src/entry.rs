b75f15bc996d5355eaa9990fcec1062aff95f94e
Mivort <lufterd@gmail.com>
2026-06-01 20:49:16 +0100

    feat(entry): show list of tags on validation error

--- src/entry.rs
+++ src/entry.rs
@@ -354,7 +354,10 @@ impl Entry {
         if !self.tags.is_empty() {
             for tag in &self.tags {
                 if !app.config.values.permit_tags.contains(tag.as_str()) {
-                    bail!("Tag should be prevent in the permitted tags list");
+                    bail!(
+                        "Tag should be present in the permitted tags list: {:?}",
+                        app.config.values.permit_tags
+                    );
                 }
             }
         }
